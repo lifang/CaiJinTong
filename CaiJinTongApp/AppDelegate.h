af6fa29cdb622bba59317d4da0687d06a5301976
david.liu <liuchong1228@yahoo.cn>
2013-10-30 16:07:27 +0800

    Initial Commit

---
+++ CaiJinTongApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CaiJinTongApp
+//
+//  Created by david on 13-10-30.
+//  Copyright (c) 2013年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CaiJinTongApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CaiJinTongApp
+//
+//  Created by david on 13-10-30.
+//  Copyright (c) 2013年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
