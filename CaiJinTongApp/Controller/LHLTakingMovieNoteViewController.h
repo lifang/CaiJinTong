05ce42b336b221a83c08160781b9a535faec2e9c
lhl0033 <lhl0033@gmail.com>
2013-12-16 13:28:55 +0800

    12.16 布局调整基本完毕

---
+++ CaiJinTongApp/Controller/LHLTakingMovieNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  LHLTakingMovieNoteViewController.h
+//  CaiJinTongApp
+//
+//  Created by apple on 13-12-11.
+//  Copyright (c) 2013年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHLTakingMovieNoteViewController : UIViewController
+
+@end
---
+++ CaiJinTongApp/Controller/LHLTakingMovieNoteViewController.m
@@ -0,0 +1,38 @@
+//
+//  LHLTakingMovieNoteViewController.m
+//  CaiJinTongApp
+//
+//  Created by apple on 13-12-11.
+//  Copyright (c) 2013年 david. All rights reserved.
+//
+
+#import "LHLTakingMovieNoteViewController.h"
+
+@interface LHLTakingMovieNoteViewController ()
+
+@end
+
+@implementation LHLTakingMovieNoteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
